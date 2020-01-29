7aa9860799949b5aac50c068f6aff1247fec75a7
RomanMysko <roman.94321@gmail.com>
2020-01-29 12:48:21 +0200

    Inserting comments into main file

--- Курсова/Prime numbers/Main.py
+++ Курсова/Prime numbers/Main.py
@@ -5,10 +5,17 @@ import random
 import time
 
 
+# A function that checks the time required for initialization of necessary elements
+# such as list of numbers which we test and a matrix of numbers generated randomly
+# in order to have same random numbers in each algorithm and
+# the time for completion of each probabilistic primality test's algorithms
+# They are Fermat's, Miller_Rabin's and Solovay_Strassen's algorithms
 def Time_test():
     print("Input the integer quantity of numbers ")
     n = int(input())
     print("Input the quantity of times to check the primality of numbers")
+    # a value that defines how much random numbers we use
+    # to improve the correctness of probabilistic algorithms
     k = int(input())
     if k <= 0:
         print("k must be > 0")
@@ -24,8 +31,6 @@ def Time_test():
             a.append(random.randint(1, random_list_of_nums[i]))
         r.append(a)
         a = []
-    '''print(random_list_of_nums)
-    print(r)'''
     end = time.time()
     print("Initialization ", end - start)
     start = time.time()
@@ -37,17 +42,24 @@ def Time_test():
     for i in range(n):
         Miller_Rabin.isPrime(random_list_of_nums[i], k, r[i])
     end = time.time()
-    print("Miller-Rabin's time - ", end - start)
+    print("Miller_Rabin's time - ", end - start)
     start = time.time()
     for i in range(n):
         Solovay_Strassen.isPrime(random_list_of_nums[i], k, r[i])
     end = time.time()
-    print("Solovay-Strassen's time - ", end - start)
+    print("Solovay_Strassen's time - ", end - start)
 
 
+# A function that allows us to determine whether or not
+# a given number is prime using all 3 algorithms;
+# True - Prime; False - Composite;
 def test():
     print("Input k>0 (probability has a direct dependency on k)")
     k = int(input())
+    if k <= 0:
+        print("k must be > 0")
+        test()
+        return
     print("Input number to check if it's prime or not")
     n = int(input())
     r = []
@@ -57,7 +69,7 @@ def test():
         r.append(random.randint(1, n))
     print("Fermat's algorithm ", Fermat.isPrime(n, k, r))
     print("Miller_Rabin's algorithm", Miller_Rabin.isPrime(n, k, r))
-    print("Solovay-Strassen's algorithm", Solovay_Strassen.isPrime(n, k, r))
+    print("Solovay_Strassen's algorithm", Solovay_Strassen.isPrime(n, k, r))
 
 
 func = input("Input 'test' to check if a number is prime or 'time' to check the performance of methods\n")
